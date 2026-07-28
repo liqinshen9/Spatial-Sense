38a98cbe571dc3a83b880ca8b8827af9f93ae51e
liqinshen9 <lshe727@aucklanduni.ac.nz>
2026-07-28 23:40:17 +1200

    ensure CORS allow both local frontend and deployed frontend

--- backend/Program.cs
+++ backend/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("http://localhost:5173", "https://spatial-sense.azurestaticapps.net")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
